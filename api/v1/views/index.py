d4e852c97ef4a2694cffb3f1c3260bec00a6d2ea
Kurrgat <mrcykj@gmail.com>
2024-05-03 01:40:30 +0300

    Task 3: Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,26 @@
 #!/usr/bin/python3
-"""
-index
-"""
+"""Returns a Json response"""
 
 from flask import jsonify
 from api.v1.views import app_views
-
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
-def status():
-    """
-    status route
-    :return: response with json
-    """
-    data = {
-        "status": "OK"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+@app_views.route('/status')
+def status_check():
+    '''Returns status code'''
+    return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
-def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """Retrieves the no of each object by type"""
+    objects = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User'),
+            }
+    return jsonify(objects)
